cd4ea539186b53bae94b5ad06f386c5f50055d0d
THANGAANANTH-J <thangamananth@gmail.com>
2017-08-16 17:05:19 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -9,29 +9,19 @@ var element = document.getElementById('main-text');
 
 element.innerHTML = 'New Value';
 
-var img1 = document.getElementById('madi');
-var img2 = document.getElementById('thanga');
-var marginLeft1 = 0;
-var marginLeft2 = 0;
+var img = document.getElementById('madi');
+
+var marginLeft = 0;
+
 
 function moveRight(){
     
-    marginLeft1 = marginLeft1 + 10;
-    img1.style.marginLeft = marginLeft1 + 'px';
-}
-
-function moveRight(){
-    
-    marginLeft2 = marginLeft2 + 10;
-    img2.style.marginLeft = marginLeft2 + 'px';
+    marginLeft = marginLeft + 10;
+    img.style.marginLeft = marginLeft + 'px';
 }
 
 img1.onclick = function fn1(){
-    var interval1 = setInterval(moveRight,100);
+    var interval = setInterval(moveRight,100);
     
 }
 
-img2.onclick = function fn2(){
-    var interval2 = setInterval(moveRight,100);
-    
-}
