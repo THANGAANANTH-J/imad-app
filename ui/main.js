1aa5f48c20c9d04a650abe9f4e54578f8d718ada
THANGAANANTH-J <thangamananth@gmail.com>
2017-08-16 17:06:59 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -16,12 +16,12 @@ var marginLeft = 0;
 
 function moveRight(){
     
-    marginLeft = marginLeft + 10;
+    marginLeft = marginLeft + 5;
     img.style.marginLeft = marginLeft + 'px';
 }
 
 img.onclick = function fn1(){
-    var interval = setInterval(moveRight,100);
+    var interval = setInterval(moveRight,50);
     
 }
 
