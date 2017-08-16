7e3e72f9481612474e2eaaf5a428177a7270b1a0
THANGAANANTH-J <thangamananth@gmail.com>
2017-08-16 22:51:33 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -20,6 +20,6 @@ button.onclick = function(){
     };
     
     // make the request
-    //request.open('GET','http://thangamananth.imad.hasura-app.io/counter',true);
-    //request.send(null);
+    request.open('GET','http://thangamananth.imad.hasura-app.io/counter',true);
+    request.send(null);
 };
