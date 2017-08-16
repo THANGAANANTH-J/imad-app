babe2b8141fc2ed362d36f2f01c71831e936915a
THANGAANANTH-J <thangamananth@gmail.com>
2017-08-16 23:21:35 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,33 @@
+ // code for counter
+
+var button = document.getElementById('counter');
+
+var counter = 0;
+
+button.onclick = function(){
+    counter = counter + 1;
+    var span = document.getElementById('count');
+    span.innerHTML = counter.toString();
+}
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
  //console.log('Loaded!');
  /*// code for counter
 
