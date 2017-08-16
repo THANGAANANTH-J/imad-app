d4087d6f7c41e53a6b30c7598c55fd967a6b572c
THANGAANANTH-J <thangamananth@gmail.com>
2017-08-16 16:01:33 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,7 +5,10 @@ function aboutme()
             document.write ("This is Thanga Ananth... I have finished ME in Embedded Systems.... ");
          }
          
-function resume()
-         {
-            document.write ("This is Thanga Ananth... I have finished ME in Embedded Systems.... ");
-         }
+var element = document.getElementById('main-text');
+element.innerHTML = 'New Value';
+
+var img = document.getElementById('madi');
+img.onclick = function(){
+    img.style.marginLeft = '100px';
+}
