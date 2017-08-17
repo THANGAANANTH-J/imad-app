354d1a14445aec77566cbc309b782055ad7f87a1
THANGAANANTH-J <thangamananth@gmail.com>
2017-08-17 14:25:42 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -34,5 +34,12 @@ submit.onclick = function(){
     
     //capture the list of names and render it as a list
     
-    //var names = {'name1','name2','name3','name4'};
+    var names = ['name1','name2','name3','name4'];
+    var list = '';
+    for(var i=0; i<names.length; i++){
+        list += '<li>' + names[i] + '</li>';
+    }
+    
+    var ul = document.getElementById('name_list');
+    ul.innerHTML = list;
 }
