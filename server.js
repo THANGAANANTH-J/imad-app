99a3382a52095cb795cbe00d875a4070d3b6eb74
THANGAANANTH-J <thangamananth@gmail.com>
2017-08-23 10:15:30 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -132,7 +132,7 @@ app.get('/submit-name',function (req, res) {
 
 app.get('articles/:fileName', function (req, res) {
     
-    pool.query("SELECT * from article where title = '"+ req.params.fileName + '"', function(err, result){
+    pool.query("SELECT * from article where title = '"+ req.params.fileName + "'", function(err, result){
         if(err){
             res.status(500).send(err.toString());
         }
