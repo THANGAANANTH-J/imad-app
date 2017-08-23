8142f11f10b0f58f019126e927ab41653d3e4e3d
THANGAANANTH-J <thangamananth@gmail.com>
2017-08-23 14:22:47 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var config = {
     database: 'thangamananth',
     host: 'db.imad.hasura-app.io',
     port: '5432',
-    password: 'db-thangamananth-9528'//process.env.DB_PASSWORD
+    password: 'db-thangamananth-95282'//process.env.DB_PASSWORD
 };
 
 var app = express();
