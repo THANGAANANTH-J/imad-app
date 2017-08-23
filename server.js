1544cc5c91b12ccb76bef0f278bde21768e75a81
THANGAANANTH-J <thangamananth@gmail.com>
2017-08-23 12:39:18 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -103,7 +103,7 @@ var pool = new Pool(config);
 
 app.get('/articles/:articleName', function (req, res) {
     
-    pool.query("SELECT * from article where title = '"+ req.params.articleName +"'", function(err, result){
+    pool.query("SELECT * from article where title = $1",[req.params.articleName], function(err, result){
         if(err){
             res.status(500).send(err.toString());
         }
