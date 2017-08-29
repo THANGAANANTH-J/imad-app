d67be426141134c0bedece77570a6472e261ceb8
THANGAANANTH-J <thangamananth@gmail.com>
2017-08-29 05:54:04 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -72,7 +72,7 @@ app.post('/create_user', function(req,res){
     
     var salt = crypto.randomBytes(128).toString('hex');
     var dbString = hash(password, salt).toSftring('hex');
-    pool.query('INSERT INTO "user" (username,password) VALUES ($1,$2)',[username,password], function(res,err){
+    pool.query('INSERT INTO "user" (username,password) VALUES ($1,$2)',[username,password], function(err,result){
         
     if(err){
         res.status(500).send(err.toString());
