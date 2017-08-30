08f0a2f51063edccc8f3f426d15b0764b012f10e
THANGAANANTH-J <thangamananth@gmail.com>
2017-08-30 06:59:52 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ var config = {
 
 var app = express();
 app.use(morgan('combined'));
-app.use(bodyparser.json());
+app.use(bodyParser.json());
 
 function createtemplate(data){
     var title = data.title;
