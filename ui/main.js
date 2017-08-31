c3c888cebfa29d903d2fcb1b69e8cf7391b6999d
THANGAANANTH-J <thangamananth@gmail.com>
2017-08-31 07:42:26 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -65,3 +65,42 @@ submit.onclick = function(){
     
    
 };
+
+
+var submit1 = document.getElementById('submit_button');
+submit1.onclick = function(){
+    
+    
+    // create a request object
+    var request = new XMLHttpRequest();
+    
+    // capture the response and store it in a variable
+    request.onreadystatechange = function(){
+        if(request.readyState === XMLHttpRequest.DONE){
+            //take some action
+            if(request.status === 200){
+                
+                alert("logged successfully");
+                
+            }else if(request.status === 403){
+                
+                alert("username/ password is incorrect");
+               
+            }else if(request.status === 500){
+                
+               alert("something went wrong in the server");
+            }
+        }
+    };
+    
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+    // make the request
+    request.open('POST','http://thangamananth.imad.hasura-app.io/login',true);
+    request.setRequestHeader("Content-Type","applicatio/json");
+    request.send(JSON.stringify({username: username, password: password}));
+    
+    
+    
+   
+};
