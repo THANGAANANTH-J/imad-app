dc5237f1fcd43031f09717c6eb89c9a1e7ab0073
THANGAANANTH-J <thangamananth@gmail.com>
2017-08-31 22:35:00 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -98,7 +98,7 @@ app.post('/login', function(req,res){
         }
         else{
             if(result.rows.length === 0){
-                res.status(403).send('username/password is invalid');
+                res.status(403).send('username/password is invalid... Response length problem');
             }
             else{
                 //var fileName = req.params.fileName;  
