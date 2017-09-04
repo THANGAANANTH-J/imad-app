86662b0336ae9dff69a529a9bd7eb62a6a8b0da5
THANGAANANTH-J <thangamananth@gmail.com>
2017-09-04 21:42:53 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -108,7 +108,7 @@ app.post('/login', function(req,res){
                 //var fileName = req.params.fileName;  
                 var dbString = result.rows[0].password;
                 var salt = dbString.split('$')[2];
-                hashedPassword = hash([password], salt);
+                hashedPassword = hash(password, salt);
                 if(hashedPassword === dbString){
                     res.send('credentials are correct');
                 }else{
