068a0c836d99c1274cb17dceb7bd9f35d7085c6e
THANGAANANTH-J <thangamananth@gmail.com>
2017-09-04 23:21:01 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -19,9 +19,7 @@ app.use(morgan('combined'));
 app.use(bodyParser.json());
 app.use(session({
     secret:'somesecretvalue', 
-    cookie: {
-        maxAge: 1000*60*60*24*30
-    }
+    cookie: { maxAge: 1000*60*60*24*30 }
 }));
 
 function createtemplate(data){
